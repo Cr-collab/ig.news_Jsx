b20369124d1d24276e7747e932098f84e905641c
Cristiano Azevdo <cristiccorrea@gmail.com>
2021-10-12 11:36:54 -0300

    alterções feitas decorrente do nosso estilo global

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { Fragment } from 'react'
+
+export default function Home() {
+  return (
+    <Fragment>
+       <Head>
+        <title>Inico | ig.news</title>
+      </Head>
+      <h1 >
+        Hello <span> World !!! </span>
+      </h1>
+    </Fragment>
+  )
+}
